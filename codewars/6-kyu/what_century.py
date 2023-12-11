e6ef76b322f08072aec47caede03674fc1ec699b
YeffyCodeGit <aditchakraborty2009@gmail.com>
2023-12-11 20:29:21 +0600

    do what century

--- codewars/6-kyu/what_century.py
+++ codewars/6-kyu/what_century.py
@@ -1,9 +1,11 @@
+import math
+
 def ordinal(n):
     # idk wtf this is thank you stack overflow
     return str(n)+("th" if 4<=n%100<=20 else {1:"st",2:"nd",3:"rd"}.get(n%10, "th"))
 
 def what_century(year):
-    century = int(year) // 100 + 1 
+    century = 1 + (int(year) - 1) // 100
     return ordinal(century)
     
-print(what_century("1234"))
+print(what_century("6700"))
