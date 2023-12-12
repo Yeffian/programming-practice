f709a6653d27bb474a607b9270f790a59d98241b
YeffyCodeGit <aditchakraborty2009@gmail.com>
2023-12-12 13:40:16 +0600

    pass in memo because im braindead

--- codewars/6-kyu/lucas_numbers.py
+++ codewars/6-kyu/lucas_numbers.py
@@ -1,7 +1,7 @@
 def lucasnum(n, memo={0:2, 1:1}):
     if n in memo: return memo[n]
     if n < 0:
-        memo[n] = lucasnum(n + 2) - lucasnum(n + 1)
+        memo[n] = lucasnum(n + 2, memo) - lucasnum(n + 1, memo)
     else:
-        memo[n] = lucasnum(n - 1) + lucasnum(n - 2)
+        memo[n] = lucasnum(n - 1, memo) + lucasnum(n - 2, memo)
     return memo[n]
