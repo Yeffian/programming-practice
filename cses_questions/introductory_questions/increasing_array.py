c7be1bd127579e8f9f6527acdd93fa3f4f7fe59e
Yeffian <aditchakraborty2009@gmail.com>
2024-03-23 15:06:34 +0800

    do increasing array

--- cses_questions/introductory_questions/increasing_array.py
+++ cses_questions/introductory_questions/increasing_array.py
@@ -1,6 +1,12 @@
 def increasing_array(n, x):
-    print(len(x))
+    moves = 0
+    for i in range(1, len(x)):
+        if x[i] < x[i - 1]:
+            moves += abs(x[i - 1] - x[i])
+            x[i] += abs(x[i - 1] - x[i])
+
+    return moves
 
 n = int(input())
 x = [int(num) for num in input().split()]
-increasing_array(n, x)
+print(increasing_array(n, x))
