d73912a7e53a1c56ea55e44209276d23c109524c
Yeffian <aditchakraborty2009@gmail.com>
2024-07-31 14:50:56 +0800

    solve unique order

---
+++ codewars/6-kyu/unique_order.py
@@ -0,0 +1,11 @@
+def unique_in_order(sequence):
+    if not sequence:
+        return []
+
+    result = [sequence[0]]
+
+    for item in sequence[1:]:
+        if item != result[-1]:
+            result.append(item)
+
+    return result
